33d064269dde22c3438de6048e39da599864e115
sangramdesai123 <sangram.desai@somaiya.edu>
2020-02-01 16:56:25 +0530

    index page

--- app.py
+++ app.py
@@ -5,26 +5,25 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def index():
-	return render_template(".html")
+	return render_template("index.html")
 
 @app.route("/login")
 def login():
 	return render_template("login.html")
 
-@app.route("/article")
-def article():
+@app.route("/rectlogin")
+def rectlogin():
+	return render_template("rectlogin.html")
 
-	title = "Rahul, Iyer show their worth as India make light work of 204 target "
-	source = "Cricbuzz"
-	author = "Cricbuzz Staff"
-	description = "Not the city, the $57 million-funded cryptocurrency custodian startup. When someone wants to keep tens or hundreds of millions of dollars in Bitcoin, Ethereum, or other coins safe, they put them in Anchorage’s vault. And now they can trade straight from custo…"
-	urlToImage = "https://techcrunch.com/wp-content/uploads/2020/01/Anchorage-Trading-1.png?w=740"
-	publishedAt = "2020-01-15T11:57:30Z"
+@app.route("/postjob")
+def postjob():
+	return render_template("demo.html")
 
-
-	return render_template("main.html", title = title, source = source, author = author, description = description, urlToImage = urlToImage, publishedAt = publishedAt)
+@app.route("/demo")
+def demo():
+	return render_template("demo.html")
 
 if __name__ == "__main__":
    	app.run(debug = True)
